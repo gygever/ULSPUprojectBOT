0cc9222d77d6c8bbfe7c832a05330fe0367c1060
gygever <toneev.toly@yandex.ru>
2023-02-20 21:21:10 +0400

    рабочий прототип

--- main.py
+++ main.py
@@ -1,24 +1,31 @@
 # main file 
 import telebot
 from config.config import BOT_API_TOKEN
+from utils.workjson import schedule
 
 
 bot = telebot.TeleBot(BOT_API_TOKEN)
+
+MESS_MAX_LENGTH = 4096
 
 @bot.message_handler(commands=['start'])
 def start(message):
     msg = bot.send_message(message.chat.id, 'Введите преподавателя, расписание которого хотите посмотреть')
     bot.register_next_step_handler(msg, prepod)
 
-@bot.message_handler(commands=['help'])
-def start(message):
-    bot.send_message(message.chat.id, 'Help command')
 
 def prepod(message):
     global prname
     prname=message.text
-    bot.send_message(message.chat.id, 'расписание')
+    raspis=schedule(prname)
+    for x in range(0, len(raspis), MESS_MAX_LENGTH):
+        shraspis = raspis[x: x + MESS_MAX_LENGTH]
+        bot.send_message(message.chat.id, shraspis)
 
+
+@bot.message_handler(commands=['help'])
+def start(message):
+    bot.send_message(message.chat.id, 'Help command')
 
 
 ## should be at the bottom, after all other function
@@ -26,7 +33,6 @@ def prepod(message):
 def any_message(message):
     msg = "Cannot understand you. Please, enter '/help' or '/start' "
     bot.reply_to(message, msg)
-
 
 
 ## load next step handlers if needed here
---
+++ utils/workjson.py
@@ -0,0 +1,11 @@
+import json
+from requests import get
+
+def schedule(prname):
+    raspis=''
+    prname = prname.replace(' ', '%20', 1)
+    req=get(f"https://raspi.ulspu.ru/json/dashboard/events?mode=teacher&value={prname}")
+    rawsch = json.loads(req.text)
+    for item in rawsch['data']:
+        raspis=raspis+item['title']+' '
+    return raspis

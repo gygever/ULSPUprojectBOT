8f60a5e7a6567459cb977a9572266eba1a165546
gygever <toneev.toly@yandex.ru>
2023-02-24 23:15:17 +0400

    +время пар

--- utils/workjson.py
+++ utils/workjson.py
@@ -1,4 +1,4 @@
-import json
+import json, datetime
 from requests import get
 
 def schedule(prname):
@@ -7,5 +7,5 @@ def schedule(prname):
     req=get(f"https://raspi.ulspu.ru/json/dashboard/events?mode=teacher&value={prname}")
     rawsch = json.loads(req.text)
     for item in rawsch['data']:
-        raspis=raspis+item['title']+'\n'
+        raspis=raspis+item['title']+' Время: '+str(datetime.datetime.strptime(item['start'], "%Y-%m-%dT%H:%M:%S.%fZ").time())[:-3]+' - '+str(datetime.datetime.strptime(item['end'], "%Y-%m-%dT%H:%M:%S.%fZ").time())[:-3]+'\n'
     return raspis

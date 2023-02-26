1321c1cd1e91168d4f96531a23e57d1b679b6e4c
gygever <toneev.toly@yandex.ru>
2023-02-27 00:01:51 +0400

    нормальное время

--- utils/workjson.py
+++ utils/workjson.py
@@ -7,5 +7,9 @@ def schedule(prname):
     req=get(f"https://raspi.ulspu.ru/json/dashboard/events?mode=teacher&value={prname}")
     rawsch = json.loads(req.text)
     for item in rawsch['data']:
-        raspis=raspis+item['title']+' Время: '+str(datetime.datetime.strptime(item['start'], "%Y-%m-%dT%H:%M:%S.%fZ").time())[:-3]+' - '+str(datetime.datetime.strptime(item['end'], "%Y-%m-%dT%H:%M:%S.%fZ").time())[:-3]+'\n'
+        startt = datetime.datetime.strptime(item['start'], "%Y-%m-%dT%H:%M:%S.%fZ").time()
+        starttime = datetime.timedelta(hours=startt.hour, minutes=startt.minute, seconds=startt.second, microseconds=startt.microsecond) + datetime.timedelta(hours=4, minutes=0, seconds=0, microseconds=0)
+        endt = datetime.datetime.strptime(item['end'], "%Y-%m-%dT%H:%M:%S.%fZ").time()
+        endtime = datetime.timedelta(hours=endt.hour, minutes=endt.minute, seconds=endt.second, microseconds=endt.microsecond) + datetime.timedelta(hours=4, minutes=0, seconds=0, microseconds=0)
+        raspis = raspis + item['title'] + ' Время: ' + str(starttime)[:-3] + '-' + str(endtime)[:-3] + '\n'
     return raspis

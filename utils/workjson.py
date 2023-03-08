15ff53bea19640f30fc1baaba862095cd2ee3ef6
gygever <toneev.toly@yandex.ru>
2023-03-09 00:49:13 +0400

    сортировка пар по времени их начала

--- utils/workjson.py
+++ utils/workjson.py
@@ -4,17 +4,21 @@ from requests import get
 def schedule(prname, day):
     raspis=''
     prname = prname.replace(' ', '%20', 1)
-    day = datetime.datetime.strptime(day, "%Y-%m-%d").date()
+    day=datetime.datetime.strptime(day, "%Y-%m-%d").date()
     req=get(f"https://raspi.ulspu.ru/json/dashboard/events?mode=teacher&value={prname}")
     rawsch = json.loads(req.text)
+    rawraspis = []
     for item in rawsch['data']:
         dayrspis = datetime.datetime.strptime(item['start'], "%Y-%m-%dT%H:%M:%S.%fZ").date()
         if day == dayrspis:
-            startt = datetime.datetime.strptime(item['start'], "%Y-%m-%dT%H:%M:%S.%fZ").time()
-            starttime = datetime.timedelta(hours=startt.hour, minutes=startt.minute, seconds=startt.second, microseconds=startt.microsecond) + datetime.timedelta(hours=4, minutes=0, seconds=0, microseconds=0)
-            endt = datetime.datetime.strptime(item['end'], "%Y-%m-%dT%H:%M:%S.%fZ").time()
-            endtime = datetime.timedelta(hours=endt.hour, minutes=endt.minute, seconds=endt.second, microseconds=endt.microsecond) + datetime.timedelta(hours=4, minutes=0, seconds=0, microseconds=0)
-            raspis = raspis + item['title'] + ' Время: ' + str(starttime)[:-3] + '-' + str(endtime)[:-3] + '\n'
+            rawraspis.append(item)
+    rawraspis = sorted(rawraspis, key=lambda x: datetime.datetime.strptime(x['start'], "%Y-%m-%dT%H:%M:%S.%fZ").time())
+    for item in rawraspis:
+        startt = datetime.datetime.strptime(item['start'], "%Y-%m-%dT%H:%M:%S.%fZ").time()
+        starttime = datetime.timedelta(hours=startt.hour, minutes=startt.minute, seconds=startt.second, microseconds=startt.microsecond) + datetime.timedelta(hours=4, minutes=0, seconds=0, microseconds=0)
+        endt = datetime.datetime.strptime(item['end'], "%Y-%m-%dT%H:%M:%S.%fZ").time()
+        endtime = datetime.timedelta(hours=endt.hour, minutes=endt.minute, seconds=endt.second, microseconds=endt.microsecond) + datetime.timedelta(hours=4, minutes=0, seconds=0, microseconds=0)
+        raspis = raspis + item['title'] + ' Время: ' + str(starttime)[:-3] + '-' + str(endtime)[:-3] + '\n'
     if len(raspis) == 0:
         raspis = f'У {prname.replace("%20", " ")} нет в занятий в этот день.'
         return raspis
